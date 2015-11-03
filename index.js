21fe33bbcc498c33ab43823b7126d65275bcc781
kirakira9 <ytlau9@outlook.com>
2015-11-04 01:29:04 +0800

    Test on adding openshift env port / ip

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function(req,res){
 	res.send("Hello World");
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(server_port, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	
