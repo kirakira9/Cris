5e424d2e33a6c3c591ae78e07b40ee30126564df
kirakira9 <ytlau9@outlook.com>
2015-11-04 01:35:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 
 app.get('/', function(req,res){
 	res.send("Hello World");
 });
 
-var server = app.listen(server_port, function(){
-	var host = server.address().address;
-	var port = server.address().port;
-	
-	console.log("Example app listening at http://%s:%s", host, port);
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
 });
